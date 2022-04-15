f4bddb33713985f71aa4bd8f7a7660cadeb32c97
Henrique Kubinhetz <rickubinhetz@gmail.com>
2022-04-15 18:20:11 -0300

    Adding documentation.

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Movie(db.Model):
     review = db.Column(db.String(500), unique=True, nullable=False)
     img_url = db.Column(db.String(500), unique=True, nullable=False)
 
-# DB Creation, should only ran once.
+# DB Creation, should only run once.
 # db.create_all()
 
 
@@ -84,24 +84,16 @@ def home():
     return render_template("index.html", movies=moviedata)
 
 
-@app.route("/add", methods=['GET', 'POST'])
-def add():
-    # Route for when user wishes to add a new movie.
-    # It consists of a form where the user inputs the movie name and
-    # the code searches for the title on the internet.
-    global tmdb_movielist
-    form = AddMovieForm()
-    if form.validate_on_submit():
-        movie_title = form.title.data
-        tmdb_movielist = tmdbAPI.search_movie(movie_title)
-        return render_template("select.html", movies=tmdb_movielist)
-    return render_template("add.html", form=form)
-
-
 @app.route("/edit/<movieid>", methods=['GET', 'POST'])
 def edit(movieid):
+    # Route for when a user wants to change the rating and review.
+    # It loads a form with these two fields,
+    # Finally, the database is updated after submission.
     form = ReviewMovieForm()
     moviedata = query_movies()
+
+    # If the form is being submitted (POST Method),
+    # the update operation below will be performed.
     if form.validate_on_submit():
         movie_rating = form.rating.data
         movie_review = form.review.data
@@ -110,20 +102,54 @@ def edit(movieid):
         movie_to_update.review = movie_review                                 # Updates the review
         db.session.commit()                                                   # Commits the update
         return redirect(url_for('home'))                                      # Redirects the user to home
+
+    # If the page is first loading (GET Method), the edit template will be rendered.
     return render_template("edit.html", movies=moviedata, id=movieid, form=form)
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    # Route for when user wishes to add a new movie.
+    # It consists of a form where the user inputs the movie name and
+    # the code searches for the title on the internet.
+    # Then, a page with all the titles within that search are displayed for selection.
+    global tmdb_movielist
+    form = AddMovieForm()
+
+    # If the form is being submitted (POST Method),
+    # it means the user is searching for a title.
+    # In that case, a list of available movies will be displayed.
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        tmdb_movielist = tmdbAPI.search_movie(movie_title)
+        return render_template("select.html", movies=tmdb_movielist)
+
+    # If the page is first loading (GET Method), the add template
+    # with the search bar will be rendered.
+    return render_template("add.html", form=form)
 
 
 @app.route("/selected/<movieid>", methods=['GET', 'POST'])
 def selected(movieid):
+    # Route for when the user selects a movie in the list displayed after the search.
+    # When one title is selected, the "edit" function is rendered.
+    # The "edit" function asks for the movie's rating and review.
+    # This is used as a first-time input for a movie being added.
 
     form = ReviewMovieForm()
+
+    # If the user submits the rating and review (POST Method), a movie entry in the DB will be created.
+    # It fetches all movie info from the base and form answers.
+    # With that, all necessary data for a new entry is available.
+    # All that remains is its organization.
     if form.validate_on_submit():
-        global tmdb_movielist
-        movie = tmdb_movielist[int(movieid)]
-        movie_rating = form.rating.data
-        movie_review = form.review.data
+        global tmdb_movielist                       # Fetching all the movies available.
+        movie = tmdb_movielist[int(movieid)]        # Selecting the right one by id.
+        movie_rating = form.rating.data             # Fetching rating from form.
+        movie_review = form.review.data             # Fetching review from form. Ready to create an entry.
 
         selected_movie = Movie(
+            # Movie structure just like the database requires.
             title=movie['title'],
             year=movie['release_date'].split('-')[0],
             description=movie['overview'],
@@ -133,15 +159,18 @@ def selected(movieid):
             img_url=f"https://image.tmdb.org/t/p/w500{movie['poster_path']}",
         )
 
-        db.session.add(selected_movie)
-        db.session.commit()
-        return redirect(url_for("home"))
+        db.session.add(selected_movie)              # Adding the movie to the DB
+        db.session.commit()                         # Commiting the changes
+        return redirect(url_for("home"))            # Redirecting the user to the homepage.
 
+    # If the page is first loading (GET Method),
+    # the form with the rating and review fields will be created.
     return render_template("edit.html", form=form)
-    # return render_template("edit.html", movies=moviedata, id=movieid, form=form)
 
 
 @app.route("/delete/<movieid>")
+# Route for when the user wishes to delete a movie (and its entry in the DB)
+# This is done by the movie's ID.
 def delete(movieid):
     movie_to_delete = Movie.query.filter_by(id=movieid).first()  # Finds the movie in the DB
     db.session.delete(movie_to_delete)                           # Deletes the movie
